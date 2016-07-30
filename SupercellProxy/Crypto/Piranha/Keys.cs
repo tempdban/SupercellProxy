654d2841b343fe8891abe9fdf3d961c107b6fb21
Knight King <kien62248@gmail.com>
2016-07-30 14:52:38 +0700

    Remove key downloader

--- SupercellProxy/Crypto/Piranha/Keys.cs
+++ SupercellProxy/Crypto/Piranha/Keys.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 
 namespace SupercellProxy
@@ -11,7 +11,7 @@ namespace SupercellProxy
         /// The generated private key, according to the modded public key.
         /// </summary>
         public static byte[] GeneratedPrivateKey
-            => Extensions.ToByteArray("1891d401fadb51d25d3a9174d472a9f691a45b974285d47729c45c6538070d85");
+            => "1891d401fadb51d25d3a9174d472a9f691a45b974285d47729c45c6538070d85".ToByteArray();
        
 
         /// <summary>
@@ -19,7 +19,7 @@ namespace SupercellProxy
         /// </summary>
         public static byte[] ModdedPublicKey
         
-            => Extensions.ToByteArray("72f1a4a4c48e44da0c42310f800e96624e6dc6a641a9d41c3b5039d8dfadc27e");
+            => "72f1a4a4c48e44da0c42310f800e96624e6dc6a641a9d41c3b5039d8dfadc27e".ToByteArray();
     
 
         /// <summary>
@@ -31,26 +31,14 @@ namespace SupercellProxy
         /// <summary>
         /// Downloads the latest public keys from InfinityModding
         /// </summary>
-        public static void DownloadPublicKey()
+        public static void PublicKey()
         {
-            try
-            {
-                if (Config.Game == Game.BOOM_BEACH)
-                    OriginalPublicKey = KeyDownloader.DownloadString("https://goo.gl/qYJSYW").ToByteArray();
-                else if (Config.Game == Game.CLASH_OF_CLANS)
-                    OriginalPublicKey = KeyDownloader.DownloadString("https://goo.gl/ubYKf1").ToByteArray();
-                else
-                    OriginalPublicKey = KeyDownloader.DownloadString("https://goo.gl/8PO34o").ToByteArray();
-
-                Logger.Log("Public key for " + Config.Game + " downloaded!");
-                Logger.Log("    => " + OriginalPublicKey.ToHexString());
-            }
-            catch(Exception ex)
-            {
-                Logger.Log("Failed to download the latest public key!");
-                Logger.Log("Please check your internet connection and contact the InfinityModding support.");
-                Program.WaitAndKill();
-            }
+            if (Config.Game == Game.BOOM_BEACH)
+                OriginalPublicKey = "680df1a0e672b908ef988332c19d3a1669c393c005516bdba648159e964a721b".ToByteArray();
+            else if (Config.Game == Game.CLASH_OF_CLANS)
+                OriginalPublicKey = "bb9ca4c6b52ecdb40267c3bcca03679201a403ef6230b9e488db949b58bc7479".ToByteArray();
+            else
+                OriginalPublicKey = "2971520277bb38734fc36e2a0d95e76e969379a5372a44c1b2fd3c1766b0016a".ToByteArray();
         }
     }
 
